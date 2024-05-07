cfb815a6ac626c2eb5628b751392524a90782011
Nikola <nikolagoev9@gmail.com>
2024-05-07 13:57:53 +0200

    Integrated basic tiled functionality

--- GXPEngine2023c/GXPEngine/MyGame.cs
+++ GXPEngine2023c/GXPEngine/MyGame.cs
@@ -1,32 +1,69 @@
-using System;                                   // System contains a lot of default C# libraries 
-using GXPEngine;                                // GXPEngine contains the engine
-using System.Drawing;                           // System.Drawing contains drawing tools such as Color definitions
+using GXPEngine;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using TiledMapParser;
 
-public class MyGame : Game {
-	public MyGame() : base(800, 600, false)     // Create a window that's 800x600 and NOT fullscreen
-	{
-		// Draw some things on a canvas:
-		EasyDraw canvas = new EasyDraw(800, 600);
-		canvas.Clear(Color.MediumPurple);
-		canvas.Fill(Color.Yellow);
-		canvas.Ellipse(width / 2, height / 2, 200, 200);
-		canvas.Fill(50);
-		canvas.TextSize(32);
-		canvas.TextAlign(CenterMode.Center, CenterMode.Center);
-		canvas.Text("Welcome!", width / 2, height / 2);
+namespace gxpengine_template.MyClasses
+{
 
-		// Add the canvas to the engine to display it:
-		AddChild(canvas);
-		Console.WriteLine("MyGame initialized");
-	}
+    public class MyGame : Game
+    {
+        public Level CurrentScene { get; private set; }
+        private string _newSceneName = null;
 
-	// For every game object, Update is called every frame, by the engine:
-	void Update() {
-		// Empty
-	}
+        public MyGame() : base(500, 800, false, false)
+        {
+            targetFps = 60;
+            LoadScene("level1.tmx");
+            OnAfterStep += LoadSceneIfNotNull;
+        }
 
-	static void Main()                          // Main() is the first method that's called when the program is run
-	{
-		new MyGame().Start();                   // Create a "MyGame" and start it
-	}
+        static void Main()
+        {
+            new MyGame().Start();
+        }
+        void Update()
+        {
+            if (Input.GetKeyDown(Key.R))
+            {
+                ReloadScene();
+            }
+        }
+        private void LoadSceneIfNotNull()
+        {
+            if (_newSceneName == null) return;
+            DestroyAll();
+            var level = new Level(_newSceneName);
+            CurrentScene = level;
+            AddChild(level);
+            level.Init();
+
+            _newSceneName = null;
+        }
+
+        public void LoadScene(string sceneName)
+        {
+            _newSceneName = sceneName;
+        }
+
+        public void ReloadScene()
+        {
+            _newSceneName = CurrentScene.Name;
+        }
+
+        protected override void OnDestroy()
+        {
+            OnAfterStep -= LoadSceneIfNotNull;
+        }
+        private void DestroyAll()
+        {
+            foreach (var child in GetChildren())
+            {
+
+                child.LateDestroy();
+
+            }
+        }
+    }
 }
