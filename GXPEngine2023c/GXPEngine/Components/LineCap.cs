5983ac3db4489ee44de3b6c7b7a01b5614ff87a2
zntKI <kaloyanivanov@hotmail.com>
2024-05-07 15:39:39 +0200

    Added line segments

---
+++ GXPEngine2023c/GXPEngine/Components/LineCap.cs
@@ -0,0 +1,17 @@
+﻿using GXPEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class LineCap
+{
+    public Vec2 position;
+
+
+    public LineCap(Vec2 position)
+    {
+        this.position = position;
+    }
+}
---
+++ GXPEngine2023c/GXPEngine/Components/LineSegment.cs
@@ -0,0 +1,22 @@
+﻿using GXPEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class LineSegment
+{
+    public Vec2 start;
+    public Vec2 end;
+    public LineCap lineCapStart;
+    public LineCap lineCapEnd;
+
+    public LineSegment(Vec2 pStart, Vec2 pEnd, bool lineCapStart = false, bool lineCapEnd = false)
+    {
+        start = pStart;
+        end = pEnd;
+        this.lineCapStart = lineCapStart ? new LineCap(pStart) : null;
+        this.lineCapEnd = lineCapEnd ? new LineCap(pEnd) : null;
+    }
+}
